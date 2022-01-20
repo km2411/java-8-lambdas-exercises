64ca958b3e49b7e420dc3aefef5f69500201a2a5
Kartik Mittal <kkartikmittal@gmail.com>
2022-01-20 19:13:53 +0530

    update sol for chapter 3

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
@@ -1,13 +1,20 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
 import com.insightfullogic.java8.examples.chapter1.Artist;
-import com.insightfullogic.java8.exercises.Exercises;
 
 import java.util.List;
 
 public class Question2 {
     // Q3
     public static int countBandMembersInternal(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+//        int totalMembers = 0;
+//        for (Artist artist : artists) {
+//            Stream<Artist> members = artist.getMembers();
+//            totalMembers += members.count();
+//        }
+        return artists.stream()
+                .map(artist -> artist.getMembers().count())
+                .reduce(0L, Long::sum)
+                .intValue();
     }
 }
--- src/main/java/com/insightfullogic/java8/exercises/chapter3/StringExercises.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/StringExercises.java
@@ -1,7 +1,5 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
-import com.insightfullogic.java8.exercises.Exercises;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -10,12 +8,16 @@ public class StringExercises {
 
     // Question 6
     public static int countLowercaseLetters(String string) {
-        return Exercises.replaceThisWithSolution();
+        return (int) string.chars()
+                .filter(Character::isLowerCase)
+                .count();
     }
 
     // Question 7
     public static Optional<String> mostLowercaseString(List<String> strings) {
-        return Exercises.replaceThisWithSolution();
+        Comparator<String> comparator = Comparator.comparing(StringExercises::countLowercaseLetters);
+        return strings.stream()
+                .max(comparator);
     }
 
 }
